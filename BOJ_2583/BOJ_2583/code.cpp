b804fcae7c8df7d78b01ae49736f2f5a891a42ef
umspirit <umjugnki@gmail.com>
2019-08-06 21:53:28 +0900

    2583 Commit

---
+++ BOJ_2583/BOJ_2583/code.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n, m, k, cnt = 0;
+int maps[101][101];
+int dx[] = { 0,1,0,-1 };
+int dy[] = { 1,0,-1,0 };
+vector<int> v;
+
+void BFS(int x, int y) {
+	queue<pair<int, int>> q;
+	int sum = 0;
+	q.push({ x, y });
+	maps[y][x] = 1;
+
+	while (q.size() > 0) {
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
+		sum++;
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[ny][nx] == 0) {
+				maps[ny][nx] = 1;
+				q.push({ nx, ny });
+			}
+		}
+	}
+
+	v.push_back(sum);
+	cnt++;
+}
+
+int main() {
+	cin >> m >> n >> k;
+
+	while (k-- > 0) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+
+		for (int i = y1; i < y2; i++) {
+			for (int j = x1; j < x2; j++) {
+				maps[i][j] = 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (maps[i][j] == 0) {
+				BFS(j, i);
+			}
+		}
+	}
+
+	cout << cnt << "\n";
+	sort(v.begin(), v.end());
+	for (int num : v)
+		cout << num << " ";
+
+	return 0;
+}
