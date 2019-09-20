d21efebf5f461b669bae68f4c6e43b7f7f2050be
umspirit <umjugnki@gmail.com>
2019-09-21 00:27:53 +0900

    백준 :

---
+++ BOJ_10809/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int arr[] = new int[26];
+
+		String s = br.readLine();
+		
+		char ch = 'a';
+		for(int j = 0; j < 26; j++) {
+			arr[j] = -1;
+			char c = (char) (j + (int)ch);
+			for(int i = 0; i < s.length(); i++) {
+				if(c == s.charAt(i)) {
+					arr[j] = i;
+					break;
+				}
+			}
+			
+			bw.write(String.valueOf(arr[j]) + " ");
+		}
+		
+		br.close();
+		bw.close();
+	}
+}
