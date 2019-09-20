a4b5e5a3660024f2f3e24a1c6bd0b1f4008e339c
umspirit <umjugnki@gmail.com>
2019-09-21 00:51:25 +0900

    백준 2908번

---
+++ BOJ_2908/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String a = st.nextToken();
+		String b = st.nextToken();
+		
+		int n1, n2;
+		
+		String tmp = "";
+		for(int i = a.length() - 1; i >= 0; i--) {
+			tmp += a.charAt(i);
+		}
+		n1 = Integer.parseInt(tmp);
+		tmp = "";
+		for(int i = b.length() - 1; i >= 0; i--) {
+			tmp += b.charAt(i);
+		}
+		n2 = Integer.parseInt(tmp);
+		
+		System.out.println(n1 < n2 ? n2 : n1);
+	}
+}
