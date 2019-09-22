0471bb5d1a538fc312b52b2aa12b89fc00ff202d
umspirit <umjugnki@gmail.com>
2019-09-22 21:52:21 +0900

    SET, MAP

--- PRG_42576/source.cpp
+++ PRG_42576/source.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <string>
 #include <algorithm>
+#include <set>
 #include <map>
 #include <vector>
 
@@ -39,3 +41,33 @@ string solution(vector<string> participant, vector<string> completion) {
 	return participant[0];
 }
 */
+
+int main() {
+	set<string> s;
+	s.insert("콜라");
+	s.insert("사이다");
+
+	map<string, int> m;
+	m["정기"] = 1;
+	m["현규"] = 3;
+
+
+
+	for (auto i = m.begin(); i != m.end(); i++) {
+		cout << i->first << " " << i->second << endl;
+	}
+
+	for (auto i : m) {
+		cout << i.first << " " << i.second << endl;
+	}
+
+	cout << m["시바"];
+
+	for (auto i : s) {
+		cout << i;
+	}
+
+	for (auto i = s.begin(); i != s.end(); i++) {
+		cout << *i;
+	}
+}
