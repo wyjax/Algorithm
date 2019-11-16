c7d21c8561472415b394ef36e023d77c44d1d8c9
wyjax <umjugnki@gmail.com>
2019-11-16 17:18:35 +0900

    삼성 SW 톱니바퀴2

--- BOJ_15662/src/Main.java
+++ BOJ_15662/src/Main.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char [][]t = new char[4][8];
+        int n = sc.nextInt();
+        char [][]t = new char[n][8];
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < n; i++) {
             t[i] = sc.next().toCharArray();
         }
 
-        int n = sc.nextInt();
+        int l = sc.nextInt();
 
-        while (n-- > 0) {
+        while (l-- > 0) {
             int no = sc.nextInt() - 1;
             int dir = sc.nextInt();
 
-            int []d = new int[4];
+            int []d = new int[n];
             d[no] = dir;
 
             for (int i = no; i > 0; i--) {
@@ -25,17 +26,16 @@ public class Main {
                     break;
             }
 
-            for (int i = no; i < 3; i++) {
+            for (int i = no; i < n - 1; i++) {
                 if (t[i][2] != t[i + 1][6])
                     d[i + 1] = -d[i];
                 else
                     break;
             }
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < n; i++) {
                 if (d[i] == 0) continue;
                 if (d[i] == 1) {
-                    // 시계 방향 회전
                     char temp = t[i][7];
                     for (int j = 7; j >= 1; j--) {
                         t[i][j] = t[i][j - 1];
@@ -43,7 +43,6 @@ public class Main {
                     t[i][0] = temp;
                 }
                 else if (d[i] == -1) {
-                    // 반시계 방향 회전
                     char temp = t[i][0];
                     for (int j = 0; j < 7; j++) {
                         t[i][j] = t[i][j + 1];
@@ -54,9 +53,9 @@ public class Main {
         }
 
         int ans = 0;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < n; i++) {
             if (t[i][0] == '1') {
-                ans += (1 << i);
+                ans ++;
             }
         }
         System.out.println(ans);
