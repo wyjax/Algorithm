27a53d57f0ada3957502b0287fe268cc2153a27e
wyjax <umjugnki@gmail.com>
2019-11-16 16:49:04 +0900

    백준 SW 톱니바퀴

--- BOJ_14891/src/Main.java
+++ BOJ_14891/src/Main.java
@@ -3,62 +3,57 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 4; // 톱니의 수
-        char[][] a = new char[4][8];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.next().toCharArray();
+        char [][]t = new char[4][8];
+
+        for (int i = 0; i < 4; i++) {
+            t[i] = sc.next().toCharArray();
         }
-        int k = sc.nextInt();
-        while (k-- > 0) {
+
+        int n = sc.nextInt();
+
+        while (n-- > 0) {
             int no = sc.nextInt() - 1;
             int dir = sc.nextInt();
-            // 각각의 톱니는 동시에 회전해야 하기 때문에
-            // 먼저, 각 톱니가 어떤 방향으로 회전해야 하는지 구하자
-            int[] d = new int[n];
+
+            int []d = new int[4];
             d[no] = dir;
-            // 왼쪽 톱니를 연쇄적으로 구하고
-            for (int i = no - 1; i >= 0; i--) {
-                if (a[i][2] != a[i + 1][6]) {
-                    // -d[i + 1] 이 현재 d[i]는 바로 왼쪽
-                    d[i] = -d[i + 1];
-                } else {
-                    // 한 톱니가 회전하지 않으면
-                    // 그 톱니의 왼쪽에 있는 톱니도 회전하지 않는다.
+
+            for (int i = no; i > 0; i--) {
+                if (t[i - 1][2] != t[i][6])
+                    d[i - 1] = -d[i];
+                else
                     break;
-                }
             }
-            // 오른쪽 톱니를 연쇄적으로 구하고
-            for (int i = no + 1; i < n; i++) {
-                if (a[i - 1][2] != a[i][6]) {
-                    d[i] = -d[i - 1];
-                } else {
-                    // 한 톱니가 회전하지 않으면
-                    // 그 톱니의 오른쪽에 있는 톱니도 회전하지 않는다.
+
+            for (int i = no; i < 3; i++) {
+                if (t[i][2] != t[i + 1][6])
+                    d[i + 1] = -d[i];
+                else
                     break;
-                }
             }
-            for (int i = 0; i < n; i++) {
+
+            for (int i = 0; i < 4; i++) {
                 if (d[i] == 0) continue;
                 if (d[i] == 1) {
-                    // 시계 방향 회전
-                    char temp = a[i][7];
+                    char temp = t[i][7];
                     for (int j = 7; j >= 1; j--) {
-                        a[i][j] = a[i][j - 1];
+                        t[i][j] = t[i][j - 1];
                     }
-                    a[i][0] = temp;
-                } else if (d[i] == -1) {
-                    // 반시계 방향 회전
-                    char temp = a[i][0];
+                    t[i][0] = temp;
+                }
+                else if (d[i] == -1) {
+                    char temp = t[i][0];
                     for (int j = 0; j < 7; j++) {
-                        a[i][j] = a[i][j + 1];
+                        t[i][j] = t[i][j + 1];
                     }
-                    a[i][7] = temp;
+                    t[i][7] = temp;
                 }
             }
         }
+
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (a[i][0] == '1') {
+        for (int i = 0; i < 4; i++) {
+            if (t[i][0] == '1') {
                 ans += (1 << i);
             }
         }
