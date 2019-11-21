cbf102eb99a4327291c24de22299c01ab674e0a7
wyjax <umjugnki@gmail.com>
2019-11-21 19:53:56 +0900

    백준 : 숨바꼭질6

---
+++ BOJ_17087/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int diff = Math.abs(x - s);
+            a[i] = diff;
+        }
+        int ans = a[0];
+        for (int i = 1; i < n; i++) {
+            ans = gcd(ans, a[i]);
+        }
+        System.out.println(ans);
+    }
+
+    static int gcd(int x, int y) {
+        if (y == 0) return x;
+        else return gcd(y, x % y);
+    }
+}
