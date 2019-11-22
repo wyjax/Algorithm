eed7a4ab3a744f21d860072db5e61423d3e32ebc
wyjax <umjugnki@gmail.com>
2019-11-22 21:26:11 +0900

    백준 : 2 x n 타일링 2

---
+++ BOJ_11727/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    /*
+       문제 : 2 x n 타일 2
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int []dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + (dp[i - 2] * 2);
+            dp[i] %= 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
