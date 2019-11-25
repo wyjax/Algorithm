04bcdc2209f9aceb919f38a2c2bfab9194746bf0
wyjax <umjugnki@gmail.com>
2019-11-25 22:14:07 +0900

    백준 : 카드 구매하기2

---
+++ BOJ_16194/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p[] = new int[n + 1];
+        int dp[] = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            p[i] = sc.nextInt();
+            dp[i] = -1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (dp[i] == -1 || dp[i] > p[j] + dp[i - j]) {
+                    dp[i] = p[j] + dp[i - j];
+                }
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
