8de4b383231ad574156296d8a9c5cc8919ddf526
wyjax <umjugnki@gmail.com>
2019-12-06 16:21:57 +0900

    백준 11055번 가장 긴 증가하는 부분 수열

---
+++ BOJ_ 11055/src/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] d = new int[n];
+
+        String[] s = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(s[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            d[i] = a[i];
+            for (int j = 0; j < i; j++) {
+                if (a[i] > a[j] && d[i] < d[j] + a[i]) {
+                    d[i] = d[j] + a[i];
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (ans < d[i])
+                ans = d[i];
+        }
+        System.out.println(ans);
+    }
+}
