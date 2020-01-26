4feb678ca36ec9de8a3960119e4ec9d553a73aee
wyjax <umjugnki@gmail.com>
2020-01-26 12:44:02 +0900

    메모리, 속도 최적화

--- ACMICPC/BOJ_15663/src/Main.java
+++ ACMICPC/BOJ_15663/src/Main.java
@@ -5,30 +5,28 @@ import java.util.*;
 
 public class Main {
     static int n, m;
-    static int[] a, ans;
+    static int[] a;
     static boolean[] check;
     static Set<String> set;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
-        set = new HashSet<>();
+        set = new TreeSet<>();
         n = Integer.parseInt(s[0]);
         m = Integer.parseInt(s[1]);
         a = new int[n];
-        ans = new int[m];
         check = new boolean[n];
 
         s = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(s[i]);
         }
-        Arrays.sort(a);
-        search(0);
+
+        search(0, "");
         List<String> list = new ArrayList<>(set);
         set.clear();
         a = null;
-        ans = null;
         check = null;
 
         Collections.sort(list, (o1, o2) -> {
@@ -53,20 +51,16 @@ public class Main {
         System.out.print(sb.toString());
     }
 
-    public static void search(int cnt) {
+    public static void search(int cnt, String str) {
         if (cnt == m) {
-            StringBuilder sb = new StringBuilder();
-            for (int n : ans)
-                sb.append(n + " ");
-            set.add(sb.toString());
+            set.add(str);
             return;
         }
 
         for (int i = 0; i < a.length; i++) {
             if (!check[i]) {
                 check[i] = true;
-                ans[cnt] = a[i];
-                search(cnt + 1);
+                search(cnt + 1, str + a[i] + " ");
                 check[i] = false;
             }
         }
