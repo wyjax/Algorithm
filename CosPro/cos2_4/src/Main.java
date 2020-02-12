e78c26d4bb2d6b0f9e0ce9aa63d38fb6daadfa2b
wyjax <umjugnki@gmail.com>
2020-02-12 19:26:58 +0900

    cos pro 1급 2-4 >> 3개 골라서 K만들기

---
+++ CosPro/cos2_4/src/Main.java
@@ -0,0 +1,34 @@
+// 다음과 같이 import를 사용할 수 있습니다.
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr, int K) {
+        // 여기에 코드를 작성해주세요.
+        int answer = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if ((arr[i] + arr[j] + arr[k]) % K == 0)
+                        answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {1, 2, 3, 4, 5};
+        int K = 3;
+        int ret = sol.solution(arr, K);
+
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    }
+}
