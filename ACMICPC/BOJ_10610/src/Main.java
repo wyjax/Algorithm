9368ddbd90103d81d767583fc8f28800f7b5e242
wyjax <umjugnki@gmail.com>
2020-02-25 13:51:50 +0900

    .백준 10610번 : 30 > 그리디 문제로 3의 배수는 각 자리합이 3의 배수인 것과 30배수는 마지막이 0이 와야한다는 것을 활함

---
+++ ACMICPC/BOJ_10610/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 30의 배수는 각 자리의 수의 합이 30의 배수가 되어야 한다.
+        // 0의 개수가 1개 이상이어야 마지막자리에 0은 무조건 와야 한다.
+
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[10];
+        int sum = 0;
+        char[] c = sc.next().toCharArray();
+
+        for (int i = 0; i < c.length; i++) {
+            a[c[i] - '0']++;
+            sum += c[i] - '0';
+        }
+        if (a[0] == 0 || sum % 3 > 0) {
+            System.out.println(-1);
+            return;
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            for (int j = 0; j < a[i]; j++) {
+                System.out.print(i);
+            }
+        }
+    }
+}
