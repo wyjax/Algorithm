0b513d02e8aa94ef3e097750bc0d461ce9a6d8e3
wyjax <umjugnki@gmail.com>
2020-03-26 10:14:43 +0900

    프로그래머스 12954번 : x만큼 간격이 있는 n개의 숫자

--- PROGRAMMERS/PRG_12954/src/Main.java
+++ PROGRAMMERS/PRG_12954/src/Main.java
@@ -1,11 +1,15 @@
-import java.nio.file.AtomicMoveNotSupportedException;
+public class Main {
+    public static void main(String[] args) {
+        new Solution().solution(-10000000, 1000);
+    }
+}
 
 class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
 
         for (int i = 1; i <= n; i++) {
-            answer[i] = i * x;
+            answer[i - 1] = (long) i * (long) x;
         }
 
         return answer;
