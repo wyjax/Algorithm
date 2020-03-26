27529e8f226494fcc022723c345f8a103553db69
wyjax <umjugnki@gmail.com>
2020-03-26 16:35:40 +0900

    백준 10814번 : 나이순 정렬

---
+++ ACMICPC/BOJ_10814/src/Main.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] a = new int[n][2];
+        String[] s = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            a[i][0] = Integer.parseInt(st.nextToken());
+            a[i][1] = i;
+            s[i] = st.nextToken();
+        }
+
+        Arrays.sort(a, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] > o2[0])
+                    return 1;
+                else if (o1[0] < o2[0])
+                    return -1;
+                return o1[1] - o2[1];
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            bw.write(a[i][0] + " " + s[a[i][1]] + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
