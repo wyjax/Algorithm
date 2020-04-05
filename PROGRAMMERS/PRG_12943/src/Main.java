2549cff41c5fd3387081945fa7a828d99a5614cf
wyjax <wyjax.dev@gmail.com>
2020-04-05 21:27:39 +0900

    프로그래머스 12943 : 콜라츠 추측

---
+++ PROGRAMMERS/PRG_12943/src/Main.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+
+        while (num != 1) {
+            if (num % 2 == 0)
+                num /= 2;
+            else
+                num = (num * 3) + 1;
+            answer++;
+
+            if (answer >= 500) {
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(626331));
+    }
+}
