2741931df8cb31e70fb661b654168d579b5fa9f4
wyjax <wyjax.dev@gmail.com>
2020-04-07 14:59:02 +0900

    Leetcode day 5

--- LeetCode/lt_day_5/src/Main.java
+++ LeetCode/lt_day_5/src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int[] a = {7, 1, 5, 3, 6, 4};
+        int[] b = {1, 2, 3, 4, 5};
+        int[] c = {5, 4, 3, 2, 1};
         System.out.println(new Solution().maxProfit(a));
     }
 }
 
 class Solution {
-    public int maxProfit(int[] prices) {
+    public int maxProfit2(int[] prices) {
         int ans = 0;
 
         for (int i = 1; i < prices.length; i++) {
@@ -18,7 +20,7 @@ class Solution {
         return ans;
     }
 
-    public int maxProfit2(int[] prices) {
+    public int maxProfit(int[] prices) {
         int max = 0, buy = 0, sell = 0;
 
         for (int i = 0; i < prices.length - 1; i++) {
@@ -26,14 +28,14 @@ class Solution {
                 sell = i + 1;
             }
             else {
-                if (sell != 0) {
+                if (sell > buy) {
                     max = max + prices[sell] - prices[buy];
                     sell = 0;
                 }
                 buy = i + 1;
             }
         }
-        if (sell != 0) {
+        if (sell > buy) {
             max = max + prices[sell] - prices[buy];
         }
         return max;
