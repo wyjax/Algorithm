bd18430a3ffed0d02c3b9bc29e89200fa37097c0
wyjax <wyjax.dev@gmail.com>
2020-04-14 23:38:13 +0900

    Leetcode 14. Longest Common Prefix

---
+++ LeetCode/Leetcode-Longest-Common-Prefix/src/Main.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0)
+            return "";
+        String obj = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(obj)) {
+                obj = obj.substring(0, obj.length() - 1);
+            }
+        }
+        return obj;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String[] s = {"a", "b"};
+        String[] s2 = {"flower", "flow", "flight"};
+        System.out.println(new Solution().longestCommonPrefix(s));
+    }
+}
+// Leetcode 14. Longest Common Prefix
