2f64b61ccc0b01303f8bee364b67d7a7bf0ee245
wyjax <wyjax.dev@gmail.com>
2020-04-19 00:28:03 +0900

    1108. Defanging an IP Address

---
+++ LeetCode/DefanginganIPAddress/src/Main.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public String defangIPaddr(String address) {
+        return (address.replace(".", "[.]"));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
