3fdebe47a4e330b30b0ec8a038254bb675f2cb71
wyjax <wyjax.dev@gmail.com>
2020-04-19 14:52:23 +0900

    141. Linked List Cycle

---
+++ LeetCode/LinkedListCycle/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+
+        return false;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
