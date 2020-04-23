868969fc21193f3d044d5787d0a35ccdc0de0e83
wyjax <wyjax.dev@gmail.com>
2020-04-23 21:27:50 +0900

    237. Delete Node in a Linked List

---
+++ LeetCode/DeleteNodeinaLinkedList/src/Main.java
@@ -0,0 +1,22 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
