36bace48b7065befe3fefb22c25ebd6ad7befbbc
wyjax <wyjax.dev@gmail.com>
2020-04-24 12:30:10 +0900

    344. Reverse String

---
+++ LeetCode/ReverseString/src/Main.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char tmp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = tmp;
+        }
+
+        for (char c : s)
+            System.out.print(c);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        new Solution().reverseString();
+    }
+}
+344. Reverse String
