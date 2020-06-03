b26692fa8576c78aafdef58923289ba87e9570ce
wyjax <wyjax.dev@gmail.com>
2020-06-04 00:36:26 +0900

    leecode may challange Counting Bits

---
+++ LeetCode/May-Leet-Coding-Challenge/CountingBits/src/Main.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+
+        for (int i = 0; i <= num; i++) {
+            ans[i] = Integer.bitCount(i);
+        }
+
+        return ans;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
